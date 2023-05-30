98e78deb72fa0b11e5e2a13d5706f231c67701d7
hibaabdulla12 <114655690+hibaabdulla12@users.noreply.github.com>
2023-05-30 19:28:39 +0530

    Add files via upload

---
+++ CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mynew1.Models;
+namespace mynew1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View(new calc());
+        }
+        [HttpPost]
+        public ActionResult Index(calc c,string calculate)
+        {
+            if(calculate=="add")
+            {
+                c.res = c.num1 + c.num2;
+            }
+            else if(calculate== "minus")
+            {
+                c.res = c.num1 - c.num2;
+            }
+            else if(calculate== "multi")
+            {
+                c.res = c.num1 * c.num2;
+            }
+            else
+            {
+                c.res = c.num1 / c.num2;
+            }
+            return View(c);
+        }
+    }
+}
---
+++ calc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mynew1.Models
+{
+    public class calc
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int res { get; set; }
+    }
+}
